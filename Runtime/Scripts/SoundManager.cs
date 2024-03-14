f7b32f7c397302d93e39ad1e008fdd5826f8bb2e
abuffie <apbuffie@gmail.com>
2024-03-13 20:57:04 -0500

    merging two projects

---
+++ Runtime/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Aarware.Core;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Script:  SoundManager.cs
+/// Created: 5/11/2023 8:52:41 AM
+/// Author:  abuffie
+/// </summary>
+
+public class SoundManager : MonoSingleton<SoundManager>{
+
+	[SerializeField] AudioMixer mixer;
+	
+	// Do not remove you will break your singleton
+	// This singleton DOES NOT destroy
+	private void Awake(){
+		base.Awake(true);
+		// Put your awake code here if any
+	}
+}
---
+++ Runtime/Scripts/Storage/SettingsData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// Script:  SettingsData.cs
+/// Created: 2/5/2024 8:32:32 PM
+/// Author:  abuffie
+/// </summary>
+
+public static class SettingsData {
+   
+   static SettingsData(){
+    
+   }
+}
